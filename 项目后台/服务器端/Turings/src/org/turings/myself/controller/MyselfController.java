1d1185cc6eb9835cddc8e1744364a8ac47dddc68
shanxinnan <2386801761qq.com>
2020-04-12 11:54:52 +0800

    shan

--- 项目后台/服务器端/Turings/src/org/turings/myself/controller/MyselfController.java
+++ 项目后台/服务器端/Turings/src/org/turings/myself/controller/MyselfController.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.turings.index.entity.School;
 import org.turings.login.entity.SendSms;
 import org.turings.myself.entity.CourseInfo;
+import org.turings.myself.entity.UserInfo;
 import org.turings.myself.service.MyselfService;
 
 @Controller
@@ -17,9 +19,45 @@ public class MyselfController {
 	@Resource
 	private MyselfService myselfService;
 	@ResponseBody
-	//这个mappingvalue的内容目前是瞎写的
-	@RequestMapping(value="/SendCourse",produces="text/json;charset=utf-8")
-	public List<CourseInfo> sendCourse() {
-		return this.myselfService.listAllCourses();
+	//显示全部课程
+	@RequestMapping(value="/GetCoursesList",produces="text/json;charset=utf-8")
+	public List<CourseInfo> sendCourse(@RequestParam(value = "uid") int uid) {
+		return this.myselfService.listAllCourses(uid);
+	}
+	//显示全部粉丝
+	@RequestMapping(value="/FansList",produces="text/json;charset=utf-8")
+	public List<UserInfo> listFan(@RequestParam(value = "uid") int uid) {
+		return this.myselfService.listAllFans(uid);
+	}
+	//显示全部关注
+	@RequestMapping(value="/AtList",produces="text/json;charset=utf-8")
+	public List<UserInfo> listAttention(@RequestParam(value = "fid") int fid) {
+		return this.myselfService.listAllAttentions(fid);
+	}
+	//添加关注
+	@RequestMapping(value="/SetAt",produces="text/json;charset=utf-8")
+	public int addAttention(@RequestParam(value = "aid") int attentionId,@RequestParam(value = "fid") int fanId) {
+		return this.myselfService.addAttentions(attentionId,fanId);
+	}
+	//编辑座右铭
+	@RequestMapping(value="/EditMotto",produces="text/json;charset=utf-8")
+	public int editMotto(@RequestParam(value = "uid") int uid,@RequestParam(value = "umotto") int uMotto) {
+		return this.myselfService.editMotto(uid,uMotto);
+	}
+	//编辑网名
+	@RequestMapping(value="/EditUname",produces="text/json;charset=utf-8")
+	public int edituName(@RequestParam(value = "uid") int uid,@RequestParam(value = "uname") int uName) {
+		return this.myselfService.edituName(uid,uName);
+	}
+	//修改头像
+	@RequestMapping(value="/InputAvatar",produces="text/json;charset=utf-8")
+	public int editAvatar(@RequestParam(value = "uid") int uid,@RequestParam(value = "uname") int uName) {
+		return this.myselfService.edituName(uid,uName);
+	}
+	//显示学校
+	@RequestMapping(value="/InputAvatar",produces="text/json;charset=utf-8")
+	public List<School> editAvat(@RequestParam(value = "uid") int uid) {
+		return this.myselfService.listSchools(uid);
 	}
 }
+
--- 项目后台/服务器端/Turings/src/org/turings/myself/service/MyselfService.java
+++ 项目后台/服务器端/Turings/src/org/turings/myself/service/MyselfService.java
@@ -6,9 +6,11 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.turings.index.entity.School;
 import org.turings.login.entity.User;
 import org.turings.myself.dao.MyselfMapper;
 import org.turings.myself.entity.CourseInfo;
+import org.turings.myself.entity.UserInfo;
 
 @Service
 @Transactional(readOnly=true)
@@ -17,8 +19,33 @@ public class MyselfService {
 	private MyselfMapper myselfMapper;
 	
 	//加载全部的课程
-	public List<CourseInfo> listAllCourses() {
-		return this.myselfMapper.listAllCourses();
+	public List<CourseInfo> listAllCourses(int uid) {
+		return this.myselfMapper.listAllCourses(uid);
 	}
+	//加载全部粉丝
+	public List<UserInfo> listAllFans(int uid) {
+		return this.myselfMapper.listAllFans(uid);
+	}
+	//加载全部关注
+	public List<UserInfo> listAllAttentions(int fid) {
+		return this.myselfMapper.listAllAttentions(fid);
+	}
+	//添加关注
+	public int addAttentions(int attentionId, int fanId) {
+		return this.myselfMapper.addAttention(attentionId,fanId);
+	}
+	//修改座右铭
+	public int editMotto(int uid, int uMotto) {
+		return this.myselfMapper.editMotto(uid,uMotto);
+	}
+	//修改网名
+	public int edituName(int uid, int uName) {
+		return this.myselfMapper.edituName(uid,uName);
+	}
+	//显示学校
+	public List<School> listSchools(int uid) {
+		return this.myselfMapper.listSchools(uid);
+	}
+	
 	
 }
