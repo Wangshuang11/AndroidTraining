8f34fc4c426f000814c35721cef2231e2159d3d4
yangliuxinns <2960095374@qq.com>
2020-06-01 19:49:42 +0800

    ylx

--- Turings/app/src/main/java/org/turings/turings/mistaken/ShowPaperActivity.java
+++ Turings/app/src/main/java/org/turings/turings/mistaken/ShowPaperActivity.java
@@ -1,5 +1,7 @@
 package org.turings.turings.mistaken;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -7,6 +9,7 @@ import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -14,6 +17,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -70,6 +74,10 @@ public class ShowPaperActivity extends AppCompatActivity implements View.OnClick
     private LinearLayout tLayout;
     private LinearLayout dLayout;
     private GifImageView gifImageView;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE };
     // 模板文集地址
     private static final String demoPath = Environment.getExternalStorageDirectory().getPath()+"/turings/";
     // 创建生成的文件地址
@@ -79,6 +87,7 @@ public class ShowPaperActivity extends AppCompatActivity implements View.OnClick
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        verifyStoragePermissions(ShowPaperActivity.this);
         setContentView(R.layout.ylx_activity_show_paper);
         getViews();
         registers();
@@ -281,7 +290,23 @@ public class ShowPaperActivity extends AppCompatActivity implements View.OnClick
         //发送通知
         sendNotification("下载成功");
     }
-
+    
+    /*
+     * android 动态权限申请
+     * */
+    public static void verifyStoragePermissions(Activity activity) {
+        try {
+            //检测是否有写的权限
+            int permission = ActivityCompat.checkSelfPermission(activity,
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有写的权限，去申请写的权限，会弹出对话框
+                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     private void sendNotification(String str) {
         Intent intent = new Intent(ShowPaperActivity.this,ViewDownloadedPapersActivity.class);
         intent.putExtra("path",path);
