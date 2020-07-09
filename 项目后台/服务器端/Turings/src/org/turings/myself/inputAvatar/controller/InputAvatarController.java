867b0e2af5e9a02cb054fab5ad5a1f4cf9983138
e <1205433325@qq.com>
2020-07-09 11:40:19 +0800

    del access key

--- 项目后台/服务器端/Turings/src/org/turings/myself/inputAvatar/controller/InputAvatarController.java
+++ 项目后台/服务器端/Turings/src/org/turings/myself/inputAvatar/controller/InputAvatarController.java
@@ -47,8 +47,8 @@ public class InputAvatarController {
 		String endpoint = "http://oss-cn-beijing.aliyuncs.com";
 		
 		// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-		String accessKeyId = "LTAI4FoQ82rmSV5EzaE1KtPU";
-		String accessKeySecret = "W8bsEiECRQfgYJJbD4rbPIdSWPaqTH";
+		String accessKeyId = "";
+		String accessKeySecret = "";
 		String bucketName = "jxy2019";
 		objectName = "avatars/i"+id+"t"+System.currentTimeMillis()+".png";
 		
