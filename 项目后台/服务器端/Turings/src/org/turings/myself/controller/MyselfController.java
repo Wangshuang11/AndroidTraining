6318d937bce21d647ced52d783e1f2c44b3dd895
e <1205433325@qq.com>
2020-07-19 01:43:22 +0800

    delete access key

--- 项目后台/服务器端/Turings/src/org/turings/myself/controller/MyselfController.java
+++ 项目后台/服务器端/Turings/src/org/turings/myself/controller/MyselfController.java
@@ -189,8 +189,8 @@ public class MyselfController {
 		// Endpoint以杭州为例，其它Region请按实际情况填写。
 		String endpoint = "http://oss-cn-beijing.aliyuncs.com";
 		// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-		String accessKeyId = "LTAI4FoQ82rmSV5EzaE1KtPU";
-		String accessKeySecret = "W8bsEiECRQfgYJJbD4rbPIdSWPaqTH";
+		String accessKeyId = "";
+		String accessKeySecret = "";
 		String bucketName = "jxy2019";
 		String objectName = "avatars/i"+uid+"t"+System.currentTimeMillis()+".png";
 		String url = "http://jxy2019.oss-cn-beijing.aliyuncs.com/"+objectName;
